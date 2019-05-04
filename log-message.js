b05a1c0c92530f881519ab7314d11e6c24f45743
Kevin Gutowski <kgutowski@berkeley.edu>
2019-05-04 11:49:39 -0700

    Update example code

--- log-message.js
+++ log-message.js
@@ -1,5 +1,12 @@
 const sketch = require('sketch')
+const Document = sketch.Document
+const currentDocument = sketch.getSelectedDocument()
 function handleURL(context) {
     let query = context.actionContext.query
-    sketch.UI.message(query.foo)
+    if (currentDocument) {
+        sketch.UI.message(query.foo)
+    } else {
+        let document = new Document()
+        sketch.UI.message(query.foo)
+    }
 }
